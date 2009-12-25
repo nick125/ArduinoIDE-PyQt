ce9a869d4c4a598ff3f9219a6640f6aa65f3eeff
pedromorgan <pedromorgan@9db9b356-f127-11de-a89a-43a960051dd3>
2009-12-25 07:59:00 +0000

    Main and a snapshot
    git-svn-id: http://arduino-pyqt.googlecode.com/svn/trunk@3 9db9b356-f127-11de-a89a-43a960051dd3

---
+++ arduino.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# copyright: Peter Morgan <pedromorgan@gmail.com>
+# License GPL
+
+import sys
+#import os
+from PyQt4 import QtCore, QtGui
+
+import gui.MainWindow
+
+if __name__ == '__main__':
+
+	app = QtGui.QApplication(sys.argv)
+
+	window = gui.MainWindow.MainWindow()
+	window.move(1300, 20)
+	window.show()
+
+	sys.exit(app.exec_())
+
---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+## This is a work around for development
+
+## path the the arduino installation
+ARDUINO_PATH = "/home/arduino/arduino-0017"
+
+SKETCHBOOKS_PATH = "/home/arduino/sketchbook"
